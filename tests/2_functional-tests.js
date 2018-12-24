c805e005b364e3a7169141a7124d5e0014bea95a
Glitch (alike-crate) <none>
2018-12-24 03:26:08 +0000

    🎡🛋 Checkpoint ./tests/2_functional-tests.js:2025103/321

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -29,12 +29,15 @@ suite('Functional Tests', function() {
         })
         .end(function(err, res){
           assert.equal(res.status, 200);
-         console.log(res.body)
           assert.equal(res.body.issue_title, 'Title')
           assert.equal(res.body.issue_text, 'text')
           assert.equal(res.body.created_by, 'Functional Test - Every field filled in')
           assert.equal(res.body.assigned_to, 'Chai and Mocha')
           assert.equal(res.body.status_text, 'In QA')
+          assert.equal(res.body.open, true)
+          assert.equal(res.body.created_on, new Date())
+          assert.equal(res.body.updated_on, new Date())
+         
 
           //fill me in too!
           
